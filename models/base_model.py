d0f329acb0825cd914fe2244afe778f7cba5176e
oyinmiebi <miebi.amangele@gmail.com>
2023-11-11 20:36:38 +0000

    [fix]: updated the base_model

--- models/base_model.py
+++ models/base_model.py
@@ -10,18 +10,18 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Initializing all attributes."""
-        dformat = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
-        if len(kwargs) != 0:
-            for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    self.__dict__[key] = datetime.strptime(value, dformat)
-                else:
-                    self.__dict__[key] = value
-        else:
+        if not kwargs:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
             models.storage.new(self)
+        else:
+            dformat = "%Y-%m-%dT%H:%M:%S.%f"
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(kwargs[key], dformat)
+                if key != '__class__':
+                    setattr(self, key, value)
 
     def save(self):
         """Update updated_at with the current datetime."""
@@ -30,16 +30,19 @@ class BaseModel:
 
     def to_dict(self):
         """Return the dictionary of the BaseModel instance."""
-        class_name = self.__class__.__name__
-        obj_dict = self.__dict__.copy()
+        new_dict = {}
 
-        # Convert datetime objects to string in ISO format
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
-        # Add class key to the dictionary
-        obj_dict['_class_'] = class_name
+        for key, values in self.__dict__.items():
+            if key == 'created_at' or key == 'updated_at':
+                new_dict[key] = values.strftime("%Y-%m-%dT%H:%M:%S.%f")
+            else:
+                if not values:
+                    pass
+                else:
+                    new_dict[key] = values
+        new_dict['__class__'] = self.__class__.__name__
 
-        return obj_dict
+        return new_dict
 
     def __str__(self):
         """Return the print/str representation of the BaseModel instance."""
---
+++ models/engine/file_storage.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" Defines the FileStorage class."""
+import json
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    """Class that serializes and deserializes JSON objects."""
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """Returns the dictionary __objects."""
+        return self.__objects
+
+    def new(self, obj):
+        """set in __objects obj with key <obj_class_name>.id"""
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        """Serialize__objects to the JSON file __file_path."""
+        odict = FileStorage.__objects
+        objdict = {obj: odict[obj].to_dict() for obj in odict.keys()}
+        with open(FileStorage.__file_path, "w") as f:
+            json.dump(objdict, f)
+
+    def reload(self):
+        """Deserialize the JSON file __file_path to __objects, if it exists."""
+        try:
+            with open(FileStorage.__file_path) as f:
+                objdict = json.load(f)
+                for o in objdict.values():
+                    cls_name = o.get("__class__")
+                    if cls_name:
+                        del o["__class__"]
+                        self.new(eval(cls_name)(**o))
+        except FileNotFoundError:
+            return
