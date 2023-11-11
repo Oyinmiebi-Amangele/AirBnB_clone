acbb0ef161ae7539f834874e9f4c77b663eda291
oyinmiebi <miebi.amangele@gmail.com>
2023-11-11 13:31:12 +0000

    [fix]: updated base model

--- models/base_model.py
+++ models/base_model.py
@@ -11,18 +11,17 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Initializing all attributes."""
         dformat = "%Y-%m-%dT%H:%M:%S.%f"
-        if kwargs:
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if len(kwargs) != 0:
             for key, value in kwargs.items():
-                if key != '__class__':
-                    if key in ['created_at', 'updated_at']:
-                        # Convert string to datetime object
-                        setattr(self, key, datetime.strptime(value, dformat))
-                    else:
-                        setattr(self, key, value)
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(value, dformat)
+                else:
+                    self.__dict__[key] = value
         else:
-            self.id = str(uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def save(self):
         """Update updated_at with the current datetime."""
